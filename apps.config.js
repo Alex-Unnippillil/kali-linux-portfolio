c03deaceae81f2e52293439b53ad65382dbf13b2
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 17:58:30 -0400

    chore: upgrade next to 15

--- apps.config.js
+++ apps.config.js
@@ -2,11 +2,8 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import ReactGA from 'react-ga4';
 
-import displaySpotify from './components/apps/spotify';
-import { displayVsCode } from './components/apps/vscode';
-=======
 import { displayX } from './components/apps/spotify';
-import displayVsCode from './components/apps/vscode';
+import { displayVsCode } from './components/apps/vscode';
 import { displaySettings } from './components/apps/settings';
 import { displayChrome } from './components/apps/chrome';
 import { displayTrash } from './components/apps/trash';
@@ -96,38 +93,23 @@ const apps = [
         desktop_shortcut: false,
         screen: displayTerminal,
     },
-      {
-          id: "x",
-          title: "X",
-          icon: './themes/Yaru/apps/x.png',
-          disabled: false,
-          favourite: true,
-          desktop_shortcut: false,
-          screen: displaySpotify, // India Top 50 Playlist 😅
-      },
-      {
-          id: "todoist",
-          title: "Todoist",
-          icon: './themes/Yaru/apps/todoist.png',
-          disabled: false,
-          favourite: false,
-          desktop_shortcut: false,
-          screen: displayTodoist,
-      },
-      {
-          id: "settings",
-          title: "Settings",
-          icon: './themes/Yaru/apps/gnome-control-center.png',
-          disabled: false,
-=======
     {
-        id: "spotify",
+        id: "x",
         title: "X",
         icon: './themes/Yaru/apps/x.png',
         disabled: false,
         favourite: true,
         desktop_shortcut: false,
         screen: displayX, // India Top 50 Playlist 😅
+    },
+    {
+        id: "todoist",
+        title: "Todoist",
+        icon: './themes/Yaru/apps/todoist.png',
+        disabled: false,
+        favourite: false,
+        desktop_shortcut: false,
+        screen: displayTodoist,
     },
     {
         id: "settings",
@@ -156,6 +138,6 @@ const apps = [
         desktop_shortcut: true,
         screen: displayGedit,
     },
-]
+];
 
 export default apps;
