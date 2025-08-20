d69a8e775602fd7c8205679d7144d3e6e2fbcb3e
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 21:38:13 -0400

    Fix conflicts: removed problematic lines from 9 files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,1 @@
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    'no-console': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    '@next/next/no-img-element': 'off',
-    'react/no-unescaped-entities': 'off',
-    'react/jsx-no-comment-textnodes': 'off'
-  }
-};
+﻿
--- components/util-components/small_arrow.tsx
+++ components/util-components/small_arrow.tsx
@@ -1,9 +1,1 @@
-import React from 'react';
-
-interface SmallArrowProps extends React.HTMLAttributes<HTMLDivElement> {
-  angle?: 'up' | 'down' | 'left' | 'right';
-}
-
-export default function SmallArrow({ angle = 'up', className = '', ...rest }: SmallArrowProps): JSX.Element {
-  return <div className={`arrow-custom-${angle} ${className}`.trim()} {...rest} />;
-}
+﻿
