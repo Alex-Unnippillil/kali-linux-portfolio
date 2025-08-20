c05488a9c194748359ce0f4eb3ee1130942c5d28
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 01:53:26 -0700

    feat: initialize Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import 'tailwindcss/tailwind.css';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const trackingId = process.env.NEXT_PUBLIC_TRACKING_ID;
+    if (trackingId) {
+      ReactGA.initialize(trackingId);
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
 
