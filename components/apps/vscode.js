decfe1c7e8b8db58f2caaad93a084d949dd091ca
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-21 00:32:35 -0700

    Embed Next.js repo in VS Code app

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,11 +3,12 @@ import React from 'react';
 export default function VsCode() {
     return (
         <iframe
-            src="https://stackblitz.com/github/Alex-Unnippillil/kali-linux-portfolio?embed=1&file=README.md"
+            src="https://stackblitz.com/github/vercel/next.js?embed=1&file=README.md"
             frameBorder="0"
             title="VsCode"
             className="h-full w-full bg-ub-cool-grey"
-            allow="accelerometer; camera; gyroscope;"
+            allow="accelerometer; camera; microphone; gyroscope; clipboard-write"
+            allowFullScreen
         ></iframe>
     );
 }
