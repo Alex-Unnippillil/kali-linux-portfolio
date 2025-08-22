aadb9f6bcf041cfc9ecef72217300e2ff0346bc6
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-22 15:21:15 -0700

    chore: upgrade tooling and modernize tests

--- __tests__/window.test.tsx
+++ __tests__/window.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { act } from 'react';
 import Window from '../components/base/window';
 
 jest.mock('react-ga4', () => ({ send: jest.fn(), event: jest.fn() }));
