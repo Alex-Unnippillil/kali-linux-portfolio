4c4ed1529a471ca40406ede5e285a86b6cb09da7
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-22 08:40:31 -0700

    feat: add app launch animations

--- components/base/ubuntu_app.js
+++ components/base/ubuntu_app.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react'
 import Image from 'next/image'
 
 export class UbuntuApp extends Component {
+    constructor() {
+        super();
+        this.state = { launching: false };
+    }
 
     openApp = () => {
+        this.setState({ launching: true }, () => {
+            setTimeout(() => this.setState({ launching: false }), 300);
+        });
         this.props.openApp(this.props.id);
     }
 
     render() {
         return (
             <div
-                className="p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-white focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white "
+                className={(this.state.launching ? " app-icon-launch " : "") + " p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-white focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white "}
                 id={"app-" + this.props.id}
                 onDoubleClick={this.openApp}
                 tabIndex={0}
