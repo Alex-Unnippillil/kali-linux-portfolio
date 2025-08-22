e6f3be1b017ce2d5d62521e10f22898e269a2b8a
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-22 16:22:21 -0700

    chore: add minimal service worker

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,7 @@
-const CACHE_NAME = 'solitaire-cache-v1';
-const URLS = ['/', '/apps/solitaire'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
