770106bba0dfd095982c201fee02d481cf754744
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-24 00:38:33 -0700

    feat(flappy-bird): add pooled engine with tap control

--- apps/flappy-bird/bird.js
+++ apps/flappy-bird/bird.js
@@ -4,7 +4,8 @@ export default class Bird {
     this.y = y;
     this.vy = 0;
     this.gravity = gravity;
-    this.radius = 10;
+    this.width = 20;
+    this.height = 20;
     this.skin = skin;
   }
 
@@ -12,10 +13,19 @@ export default class Bird {
     this.vy = force;
   }
 
-  update(reverse = false) {
+  update(reverse = false, dt = 1) {
     const g = reverse ? -this.gravity : this.gravity;
-    this.vy += g;
-    this.y += this.vy;
+    this.vy += g * dt;
+    this.y += this.vy * dt;
+  }
+
+  get bounds() {
+    return {
+      left: this.x - this.width / 2,
+      right: this.x + this.width / 2,
+      top: this.y - this.height / 2,
+      bottom: this.y + this.height / 2,
+    };
   }
 
   setSkin(color) {
@@ -24,8 +34,11 @@ export default class Bird {
 
   draw(ctx) {
     ctx.fillStyle = this.skin;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fill();
+    ctx.fillRect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
   }
 }
