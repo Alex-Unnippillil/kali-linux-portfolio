e7a07e3400a93826ea00029a2c2b81e3ec78a163
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-24 07:10:16 -0700

    test: cover flappy bird mechanics

--- apps/flappy-bird/bird.js
+++ apps/flappy-bird/bird.js
@@ -2,10 +2,13 @@ export default class Bird {
   constructor(x, y, gravity, skin = 'yellow') {
     this.x = x;
     this.y = y;
+    // keep sub-pixel position separate from draw position
+    this.yPos = y;
     this.vy = 0;
     this.gravity = gravity;
     this.width = 20;
     this.height = 20;
+    this.hitboxPadding = 2;
     this.skin = skin;
   }
 
@@ -13,18 +16,22 @@ export default class Bird {
     this.vy = force;
   }
 
-  update(reverse = false, dt = 1) {
+  update(reverse = false) {
+    // apply gravity in fixed steps for consistent behaviour
     const g = reverse ? -this.gravity : this.gravity;
-    this.vy += g * dt;
-    this.y += this.vy * dt;
+    this.vy += g;
+    this.yPos += this.vy;
+    // sync draw position with sub-pixel value
+    this.y = this.yPos;
   }
 
   get bounds() {
+    const pad = this.hitboxPadding;
     return {
-      left: this.x - this.width / 2,
-      right: this.x + this.width / 2,
-      top: this.y - this.height / 2,
-      bottom: this.y + this.height / 2,
+      left: this.x - this.width / 2 + pad,
+      right: this.x + this.width / 2 - pad,
+      top: this.y - this.height / 2 + pad,
+      bottom: this.y + this.height / 2 - pad,
     };
   }
 
