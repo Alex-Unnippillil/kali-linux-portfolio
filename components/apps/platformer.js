c82a3e2e952c7955aa96914a76a6f775e09d90c5
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-24 08:37:06 -0700

    feat: add pixi platformer game

--- components/apps/platformer.js
+++ components/apps/platformer.js
@@ -1,12 +1,1 @@
-import React from 'react';
-
-const Platformer = () => (
-  <iframe
-    src="/apps/platformer/index.html"
-    title="Platformer"
-    className="w-full h-full"
-    frameBorder="0"
-  ></iframe>
-);
-
-export default Platformer;
+export { default } from '../../apps/platformer';
