27afa61d51eec8352f972c78028bb6b8df063a40
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-24 08:08:49 -0700

    Add settings app with server storage and JSON import/export

---
+++ pages/apps/settings.tsx
@@ -0,0 +1,7 @@
+import dynamic from 'next/dynamic';
+
+const SettingsApp = dynamic(() => import('../../apps/settings'), { ssr: false });
+
+export default function SettingsPage() {
+  return <SettingsApp />;
+}
