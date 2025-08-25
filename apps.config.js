a2c63cd5c064e4a8f65d8418736be9e4faf634a1
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-25 04:55:41 -0700

    Add John the Ripper app

--- apps.config.js
+++ apps.config.js
@@ -78,6 +78,7 @@ const CandyCrushApp = createDynamicApp('candy-crush', 'Candy Crush');
 
 const GomokuApp = createDynamicApp('gomoku', 'Gomoku');
 const PinballApp = createDynamicApp('pinball', 'Pinball');
+const JohnApp = createDynamicApp('john', 'John the Ripper');
 
 
 const displayTerminal = createDisplay(TerminalApp);
@@ -117,6 +118,7 @@ const displayCandyCrush = createDisplay(CandyCrushApp);
 const displayGomoku = createDisplay(GomokuApp);
 
 const displayPinball = createDisplay(PinballApp);
+const displayJohn = createDisplay(JohnApp);
 
 
 // Default window sizing for games to prevent oversized frames
@@ -603,6 +605,15 @@ const apps = [
     favourite: false,
     desktop_shortcut: false,
     screen: displayWeather,
+  },
+  {
+    id: 'john',
+    title: 'John the Ripper',
+    icon: './themes/Yaru/apps/john.svg',
+    disabled: false,
+    favourite: false,
+    desktop_shortcut: false,
+    screen: displayJohn,
   },
   // Games are included so they appear alongside apps
   ...games,
