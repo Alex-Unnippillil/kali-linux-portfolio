9be8c4f349f9766445ec6aa1213ae6c2959c3aee
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-26 00:24:40 -0700

    Allow custom figlet fonts

--- components/apps/figlet/worker.js
+++ components/apps/figlet/worker.js
@@ -6,7 +6,14 @@ figlet.parseFont('Standard', standard);
 figlet.parseFont('Slant', slant);
 
 self.onmessage = (e) => {
-  const { text, font } = e.data;
-  const rendered = figlet.textSync(text || '', { font });
-  self.postMessage(rendered);
+  const { type } = e.data;
+  if (type === 'parseFont') {
+    const { name, data } = e.data;
+    figlet.parseFont(name, data);
+    self.postMessage({ type: 'fontParsed', name });
+  } else if (type === 'render') {
+    const { text, font } = e.data;
+    const rendered = figlet.textSync(text || '', { font });
+    self.postMessage({ type: 'render', output: rendered });
+  }
 };
