c63c47684f6c34ff8c940b725d7e684ed6f4f691
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-26 18:26:43 -0700

    chore: add default Spotify playlist

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import tracks from './spotify-playlist.json';
 
 export default function SpotifyApp() {
+  const [mini, setMini] = useState(false);
+
   return (
-    <div className="h-full w-full bg-ub-cool-grey">
-      <iframe
-        src="https://open.spotify.com/embed/playlist/37i9dQZF1E37fa3zdWtvQY?utm_source=generator&theme=0"
-        title="Daily Mix 2"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      />
+    <div className="h-full w-full bg-ub-cool-grey text-white flex flex-col">
+      <div className="p-2 flex justify-end">
+        <button
+          className="px-2 py-1 bg-ub-grey text-xs rounded"
+          onClick={() => setMini((m) => !m)}
+        >
+          {mini ? 'Full Player' : 'Mini Player'}
+        </button>
+      </div>
+      <div className="flex-1 overflow-y-auto space-y-2 p-2">
+        {tracks.map((id) => (
+          <iframe
+            key={id}
+            src={`https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0`}
+            width="100%"
+            height={mini ? '80' : '152'}
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
-export const displaySpotify = () => <SpotifyApp />;
-
