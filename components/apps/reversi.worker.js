f9e6d5b1a10ef1449b6dd25ab4d44250a8651c9a
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-26 22:52:51 -0700

    feat(reversi): add mcts ai and heatmap hints

--- components/apps/reversi.worker.js
+++
@@ -1,7 +0,0 @@
-import { bestMove } from './reversiLogic';
-
-onmessage = (e) => {
-  const { board, player, depth } = e.data;
-  const move = bestMove(board, player, depth);
-  postMessage({ move });
-};
