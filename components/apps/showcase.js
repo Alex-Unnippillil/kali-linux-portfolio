6cee4e58c27e9272bce9f82bc225017b08f40584
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-26 22:43:12 -0700

    refactor: split utilities from games

---
+++ components/apps/showcase.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Showcase = () => (
+  <div className="w-full h-full p-4 bg-ub-cool-grey text-white overflow-auto">
+    <p>Showcase placeholder</p>
+  </div>
+);
+
+export default Showcase;
+
+export const displayShowcase = () => <Showcase />;
+
