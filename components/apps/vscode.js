b42b9d983c20c9e31d8b13b500c6772ce5203ddf
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-27 06:46:16 -0700

    feat(vscode): secure lazy iframe

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExternalFrame from '../ExternalFrame';
 
 export default function VsCode() {
+    const [loaded, setLoaded] = useState(false);
+
     return (
-        <ExternalFrame
-            src="https://stackblitz.com/github/Alex-Unnippillil/kali-linux-portfolio?embed=1&file=README.md"
-            title="VsCode"
-            className="h-full w-full bg-ub-cool-grey"
-            allow="accelerometer; camera; microphone; gyroscope; clipboard-write"
-            allowFullScreen
-            frameBorder="0"
-            prefetch={false}
-        />
+        <div className="relative h-full w-full">
+            {!loaded && (
+                <div className="absolute inset-0 animate-pulse bg-ub-cool-grey" />
+            )}
+            <ExternalFrame
+                src="https://stackblitz.com/github/Alex-Unnippillil/kali-linux-portfolio?embed=1&file=README.md"
+                title="VsCode"
+                className={`h-full w-full bg-ub-cool-grey ${loaded ? 'block' : 'hidden'}`}
+                sandbox="allow-scripts allow-same-origin"
+                allow="clipboard-write"
+                allowFullScreen
+                frameBorder="0"
+                prefetch={false}
+                loading="lazy"
+                onLoad={() => setLoaded(true)}
+            />
+        </div>
     );
 }
 
