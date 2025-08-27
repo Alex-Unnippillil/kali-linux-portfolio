8beffb8856c1e5050c5238ccbccbba73479b7b47
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-26 22:50:24 -0700

    Add Supabase realtime leaderboard

--- pages/apps/phaser_matter.tsx
+++ pages/apps/phaser_matter.tsx
@@ -1,7 +1,13 @@
 import dynamic from 'next/dynamic';
+import Leaderboard from '../../components/Leaderboard';
 
 const PhaserMatter = dynamic(() => import('../../apps/phaser_matter'), { ssr: false });
 
 export default function PhaserMatterPage() {
-  return <PhaserMatter />;
+  return (
+    <div className="space-y-4">
+      <Leaderboard game="phaser_matter" />
+      <PhaserMatter />
+    </div>
+  );
 }
