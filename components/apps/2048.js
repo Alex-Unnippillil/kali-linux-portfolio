5239864c06d6a5349b1021e7a0cc91e9f9a082ee
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-27 07:13:10 -0700

    refactor: simplify 2048 imports

--- components/apps/2048.js
+++ components/apps/2048.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import { useEffect, useCallback, useState } from 'react';
 import usePersistentState from '../../hooks/usePersistentState';
 import GameLayout from './GameLayout';
 import useGameControls from './useGameControls';
