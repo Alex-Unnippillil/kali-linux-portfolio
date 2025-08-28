7a80090cf030684bf60292888878570124a422e3
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 11:11:08 -0700

    Add preconnect and preload links for fonts and images

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,6 +16,30 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.webmanifest" />
           <meta name="theme-color" content="#0f1317" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+            as="style"
+            crossOrigin="anonymous"
+            fetchPriority="high"
+          />
+          <link
+            rel="preload"
+            href="/images/logos/logo.png"
+            as="image"
+            fetchPriority="high"
+          />
+          <link
+            rel="preload"
+            href="/images/logos/bitmoji.png"
+            as="image"
+          />
           <Script src="/theme.js" strategy="beforeInteractive" />
         </Head>
         <body>
