04cfc52b9834e3b3d9aae0aebc4a95f29cb1ddfd
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 11:11:54 -0700

    feat: add skip link and landmark roles

--- components/screen/side_bar.js
+++ components/screen/side_bar.js
@@ -27,7 +27,10 @@ export default function SideBar(props) {
 
     return (
         <>
-            <div className={(props.hide ? " -translate-x-full " : "") + " absolute transform duration-300 select-none z-40 left-0 top-0 h-full pt-7 w-auto flex flex-col justify-start items-center border-black border-opacity-60 bg-black bg-opacity-50"}>
+            <nav
+                aria-label="Dock"
+                className={(props.hide ? " -translate-x-full " : "") + " absolute transform duration-300 select-none z-40 left-0 top-0 h-full pt-7 w-auto flex flex-col justify-start items-center border-black border-opacity-60 bg-black bg-opacity-50"}
+            >
                 {
                     (
                         Object.keys(props.closed_windows).length !== 0
@@ -36,7 +39,7 @@ export default function SideBar(props) {
                     )
                 }
                 <AllApps showApps={props.showAllApps} />
-            </div>
+            </nav>
             <div onMouseEnter={showSideBar} onMouseLeave={hideSideBar} className={"w-1 h-full absolute top-0 left-0 bg-transparent z-50"}></div>
         </>
     )
