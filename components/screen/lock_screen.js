b57cdc09d3b6d091376ad9eb0b474c0c41a1058b
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 11:05:56 -0700

    chore: add content-visibility to hidden panels

--- components/screen/lock_screen.js
+++ components/screen/lock_screen.js
@@ -12,7 +12,10 @@ export default function LockScreen(props) {
     };
 
     return (
-        <div id="ubuntu-lock-screen" style={{ zIndex: "100" }} className={(props.isLocked ? " visible translate-y-0 " : " invisible -translate-y-full ") + " absolute outline-none bg-black bg-opacity-90 transform duration-500 select-none top-0 right-0 overflow-hidden m-0 p-0 h-screen w-screen"}>
+        <div
+            id="ubuntu-lock-screen"
+            style={{ zIndex: "100", contentVisibility: 'auto' }}
+            className={(props.isLocked ? " visible translate-y-0 " : " invisible -translate-y-full ") + " absolute outline-none bg-black bg-opacity-90 transform duration-500 select-none top-0 right-0 overflow-hidden m-0 p-0 h-screen w-screen"}>
             <div style={{ backgroundImage: `url(/wallpapers/${wallpaper}.webp)`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPositionX: "center" }} className="absolute top-0 left-0 w-full h-full transform z-20 blur-md "></div>
             <div className="w-full h-full z-50 overflow-hidden relative flex flex-col justify-center items-center text-white">
                 <div className=" text-7xl">
