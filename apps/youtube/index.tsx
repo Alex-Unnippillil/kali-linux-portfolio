c5468bb4c539fc24f7293e692244cec625e6c62e
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-29 11:45:52 -0700

    style: modernize youtube app

--- apps/youtube/index.tsx
+++ apps/youtube/index.tsx
@@ -1,3 +1,11 @@
 'use client';
+
 import YouTubeApp from '../../components/apps/youtube';
-export default YouTubeApp;
+
+export default function YouTubePage() {
+  return (
+    <div className="h-full w-full bg-ub-dark-grey font-sans text-ubt-cool-grey">
+      <YouTubeApp />
+    </div>
+  );
+}
