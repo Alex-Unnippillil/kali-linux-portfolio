72df3183bd4d47de49eedb854dfc4c10841cc95e
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 22:10:10 -0700

    feat: add doc pip media controller extension

---
+++ chrome-extension/pip.js
@@ -0,0 +1,28 @@
+const omnibox = document.getElementById('omnibox');
+const playBtn = document.getElementById('play');
+const pauseBtn = document.getElementById('pause');
+
+function refresh() {
+  chrome.runtime.sendMessage({ type: 'query' }, (response) => {
+    if (!Array.isArray(response)) return;
+    const anyPlaying = response.some((tab) => tab.medias.some((m) => m.playing));
+    playBtn.disabled = anyPlaying;
+    pauseBtn.disabled = !anyPlaying;
+  });
+}
+
+playBtn.addEventListener('click', () => {
+  chrome.runtime.sendMessage({ type: 'control', action: 'play' });
+});
+
+pauseBtn.addEventListener('click', () => {
+  chrome.runtime.sendMessage({ type: 'control', action: 'pause' });
+});
+
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.type === 'refresh') {
+    refresh();
+  }
+});
+
+refresh();
