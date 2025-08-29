76135aa0f82f72e38a3f9e989b89f1e46e3f0564
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-29 13:09:58 -0700

    feat(qr): store last scans and generations

---
+++ __tests__/qrLast.test.ts
@@ -0,0 +1,22 @@
+import {
+  loadLastGeneration,
+  loadLastScan,
+  saveLastGeneration,
+  saveLastScan,
+} from '../utils/qrStorage';
+
+describe('qr last storage', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('saves and loads last scan', () => {
+    saveLastScan('foo');
+    expect(loadLastScan()).toBe('foo');
+  });
+
+  it('saves and loads last generation', () => {
+    saveLastGeneration('bar');
+    expect(loadLastGeneration()).toBe('bar');
+  });
+});
