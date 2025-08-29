2b9d70b1715cd1dd710cfccb2f265eb9c0273b41
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-29 01:21:45 -0700

    feat: expose module catalog component

--- apps/metasploit/index.tsx
+++ apps/metasploit/index.tsx
@@ -2,9 +2,15 @@
 
 import React from 'react';
 import MetasploitApp from '../../components/apps/metasploit';
+import ModuleCatalog from './components/ModuleCatalog';
 
 const MetasploitPage: React.FC = () => {
-  return <MetasploitApp />;
+  return (
+    <div>
+      <MetasploitApp />
+      <ModuleCatalog />
+    </div>
+  );
 };
 
 export default MetasploitPage;
