1a52643f16db98fae24ba014fdc9e1227086cc70
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-29 03:59:43 -0700

    feat: add platformer checkpoints

--- apps/phaser_matter/gameLogic.ts
+++ apps/phaser_matter/gameLogic.ts
@@ -19,13 +19,21 @@ export class GameState {
   }
 
   /**
+   * Returns the current respawn position, preferring the last checkpoint
+   * if one has been recorded.
+   */
+  getRespawnPoint(): Point {
+    return this.checkpoint ? { ...this.checkpoint } : { ...this.spawn };
+  }
+
+  /**
    * Returns the position a player should respawn to when falling below
    * the provided boundary. If the player is above the boundary their
    * current position is returned.
    */
   respawnIfOutOfBounds(player: Point, boundaryY: number): Point {
     if (player.y > boundaryY) {
-      return this.checkpoint ? { ...this.checkpoint } : { ...this.spawn };
+      return this.getRespawnPoint();
     }
     return player;
   }
