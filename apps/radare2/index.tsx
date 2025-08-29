13d5a55cbcddbd5eab1269c5ab2fb1d3ea89ad9a
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 18:14:30 -0700

    feat: add radare2 app with analysis views

---
+++ apps/radare2/index.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+import Radare2 from '../../components/apps/radare2';
+import sample from './sample.json';
+
+const Radare2Page: React.FC = () => {
+  return <Radare2 initialData={sample} />;
+};
+
+export default Radare2Page;
