e95eed668a175fb34fcdf5758c1c9ea81b7df111
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 18:02:54 -0700

    feat: add volatility analysis views

---
+++ apps/volatility/index.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import VolatilityApp from '../../components/apps/volatility';
+
+const VolatilityPage: React.FC = () => {
+  return <VolatilityApp />;
+};
+
+export default VolatilityPage;
