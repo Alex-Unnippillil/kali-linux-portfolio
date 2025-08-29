4736aec5cf7948f89553a2044d23300d878405d0
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-29 03:56:59 -0700

    Add tag-based filtering for post-exploitation modules

---
+++ modules/metadata.ts
@@ -0,0 +1,58 @@
+export interface ModuleOption {
+  name: string;
+  required: boolean;
+  description: string;
+}
+
+export interface ModuleMetadata {
+  name: string;
+  description: string;
+  tags: string[];
+  options: ModuleOption[];
+}
+
+const modules: ModuleMetadata[] = [
+  {
+    name: 'getsystem',
+    description: 'Attempt to elevate your privilege to that of local system.',
+    tags: ['privilege', 'elevation'],
+    options: [
+      {
+        name: 'SESSION',
+        required: true,
+        description: 'The session to run this module on.',
+      },
+    ],
+  },
+  {
+    name: 'keyscan_start',
+    description: 'Start capturing keystrokes.',
+    tags: ['keylogging'],
+    options: [
+      {
+        name: 'SESSION',
+        required: true,
+        description: 'The session to run this module on.',
+      },
+    ],
+  },
+  {
+    name: 'persistence_service',
+    description: 'Achieve persistence by installing a service.',
+    tags: ['persistence', 'service'],
+    options: [
+      {
+        name: 'SESSION',
+        required: true,
+        description: 'The session to run this module on.',
+      },
+      {
+        name: 'RPORT',
+        required: false,
+        description: 'Remote port used for callback.',
+      },
+    ],
+  },
+];
+
+export default modules;
