0b6bcfe56dac0a4704d99965321cae0fd11be8bf
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-30 02:14:50 -0700

    Replace deprecated flags API with custom handler

--- pages/api/flags/index.ts
+++ pages/api/flags/index.ts
@@ -1,6 +1,20 @@
-import { getProviderData, createFlagsEndpoint } from 'flags/next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { verifyAccess, version, type ProviderData } from 'flags';
+import { getProviderData } from 'flags/next';
 import * as appFlags from '../../flags';
 
-export default createFlagsEndpoint(async (req, res) => {
-  res.status(200).json(await getProviderData(appFlags));
-});
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const ok = await verifyAccess(req.headers.authorization as string);
+  if (!ok) {
+    res.status(401).json(null);
+    return;
+  }
+
+  const data: ProviderData = await getProviderData(appFlags);
+  res.setHeader('x-flags-sdk-version', version);
+  res.status(200).json(data);
+}
+
