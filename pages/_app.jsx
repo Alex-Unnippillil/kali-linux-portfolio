9efeb63d2d138dd0055131be276de19714a374f3
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-29 18:16:38 -0700

    chore: drop unsupported React19 packages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,7 +8,6 @@ import '../styles/index.css';
 import '../styles/resume-print.css';
 import '../styles/print.css';
 import '@xterm/xterm/css/xterm.css';
-import 'leaflet/dist/leaflet.css';
 import { SettingsProvider } from '../hooks/useSettings';
 import ShortcutOverlay from '../components/common/ShortcutOverlay';
 import PipPortalProvider from '../components/common/PipPortal';
