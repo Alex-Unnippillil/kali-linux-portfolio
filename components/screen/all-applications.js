2af1b8d6995e7ff5602297002b23454c6a286106
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-04 16:41:40 -0700

    feat: add whisker menu shortcuts

--- components/screen/all-applications.js
+++ components/screen/all-applications.js
@@ -55,10 +55,11 @@ class AllApplications extends React.Component {
 
     render() {
         return (
-            <div className="fixed inset-0 z-50 flex flex-col items-center overflow-y-auto bg-ub-grey bg-opacity-95 all-apps-anim">
+            <div id="whisker-menu" className="fixed inset-0 z-50 flex flex-col items-center overflow-y-auto bg-ub-grey bg-opacity-95 all-apps-anim">
                 <input
                     className="mt-10 mb-8 w-2/3 md:w-1/3 px-4 py-2 rounded bg-black bg-opacity-20 text-white focus:outline-none"
                     placeholder="Search"
+                    aria-label="Search applications"
                     value={this.state.query}
                     onChange={this.handleChange}
                 />
