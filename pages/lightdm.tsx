5a51893798c6ed4b8861743778c3e8161ee0f0f6
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-05 22:24:48 -0700

    Auto-merge PR #4274 - feat: add LightDM explainer pane
    Force merged PR #4274

---
+++ pages/lightdm.tsx
@@ -0,0 +1,10 @@
+import LightDMPane from '../components/LightDMPane';
+
+export default function LightDMPage() {
+  return (
+    <div className="min-h-screen flex">
+      <LightDMPane />
+    </div>
+  );
+}
+
