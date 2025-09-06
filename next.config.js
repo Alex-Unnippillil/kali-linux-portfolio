f8bb4321e57878311a1b36c7bc70031280aa6f8d
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-06 01:31:35 -0700

    Auto-merge PR #4661 - Add local image patterns for app icon directories
    Force merged PR #4661

--- next.config.js
+++ next.config.js
@@ -155,6 +155,10 @@ module.exports = withBundleAnalyzer(
         'data.typeracer.com',
         'images.credly.com',
       ],
+      localPatterns: [
+        { pathname: '/themes/Yaru/apps/**' },
+        { pathname: '/icons/**' },
+      ],
       deviceSizes: [640, 750, 828, 1080, 1200, 1280, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256],
     },
