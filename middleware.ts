ebd408f976dfa30f9e3329867b161b41e84784e3
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-06 03:02:44 -0700

    Auto-merge PR #4708 - chore: add strict referrer policy header
    Force merged PR #4708

--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,6 @@ export function middleware(req: NextRequest) {
   const res = NextResponse.next();
   res.headers.set('x-csp-nonce', n);
   res.headers.set('Content-Security-Policy', csp);
+  res.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   return res;
 }
