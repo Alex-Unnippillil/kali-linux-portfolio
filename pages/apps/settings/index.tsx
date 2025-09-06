2b0430889e05e0c3542c742219fceb2bcc12a0ac
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-05 20:50:45 -0700

    feat(settings): add removable media banner

---
+++ pages/apps/settings/index.tsx
@@ -0,0 +1,9 @@
+import dynamic from "next/dynamic";
+
+const SettingsApp = dynamic(() => import("../../../apps/settings"), {
+  ssr: false,
+});
+
+export default function SettingsPage() {
+  return <SettingsApp />;
+}
