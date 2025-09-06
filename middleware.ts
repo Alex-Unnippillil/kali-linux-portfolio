ef30917f3f9457adbf2fa0a164f25d32f397716c
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-06 16:24:18 -0700

    Auto-merge PR #4758 - Add matcher to middleware to skip static assets
    Force merged PR #4758

--- middleware.ts
+++ middleware.ts
@@ -34,3 +34,9 @@ export function middleware(req: NextRequest) {
 
   return res;
 }
+
+export const config = {
+  matcher: [
+    '/((?!public|_next/static|_next/image|favicon\.ico|robots\.txt|sitemap\.xml|manifest\.webmanifest|sw\.js|workbox-.*\.js).*)'
+  ]
+};
