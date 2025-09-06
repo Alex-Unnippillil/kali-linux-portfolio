936daf543c3c4da90a7e9dae4ee95df8974197ad
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-05 22:23:08 -0700

    Auto-merge PR #4302 - feat: add randomized IOPS bar
    Force merged PR #4302

--- apps/resource-monitor/index.tsx
+++ apps/resource-monitor/index.tsx
@@ -2,10 +2,14 @@
 
 import React from 'react';
 import NetworkInsights from './components/NetworkInsights';
+import IOPSBar from './components/IOPSBar';
 
 export default function ResourceMonitorApp() {
   return (
-    <div className="h-full w-full bg-ub-cool-grey overflow-auto">
+    <div className="h-full w-full bg-ub-cool-grey overflow-auto p-2">
+      <div className="mb-4 flex justify-center">
+        <IOPSBar />
+      </div>
       <NetworkInsights />
     </div>
   );
