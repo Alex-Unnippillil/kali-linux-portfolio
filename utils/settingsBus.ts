8b3428e73d3dcf2c0ebde7b35b70735501bef2f9
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-05 22:20:53 -0700

    Auto-merge PR #4370 - feat: add settings change log overlay
    Force merged PR #4370

---
+++ utils/settingsBus.ts
@@ -0,0 +1,22 @@
+'use client';
+
+import { publish as pub, subscribe as sub } from './pubsub';
+
+export type SettingsMessage = {
+  channel: string;
+  property: string;
+  value: unknown;
+};
+
+const TOPIC = 'settings';
+
+export const publish = (channel: string, property: string, value: unknown): void => {
+  pub(TOPIC, { channel, property, value });
+};
+
+export const subscribe = (handler: (msg: SettingsMessage) => void): (() => void) =>
+  sub(TOPIC, handler);
+
+const settingsBus = { publish, subscribe };
+
+export default settingsBus;
