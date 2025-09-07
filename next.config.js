8e248b6241e2bd5ea321330c8231461419185a28
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-07 15:11:48 -0700

    Extend cache duration for images

--- next.config.js
+++ next.config.js
@@ -221,7 +221,7 @@ module.exports = withBundleAnalyzer(
                 headers: [
                   {
                     key: 'Cache-Control',
-                    value: 'public, max-age=86400, immutable',
+                    value: 'public, max-age=31536000, immutable',
                   },
                 ],
               },
