8b0b9476695f06f0a02859eb6c1b10ed76dccc8b
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-08 09:31:00 -0700

    Auto-merge PR #5158 - feat: add policy index grid
    Force merged PR #5158

---
+++ data/policies.ts
@@ -0,0 +1,69 @@
+export interface PolicyLink {
+  label: string;
+  href: string;
+  author: string;
+  updated: string;
+}
+
+export const policies: PolicyLink[] = [
+  {
+    label: "Cookie Policy",
+    href: "https://www.kali.org/docs/policy/cookie/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali Linux EULA",
+    href: "https://www.kali.org/docs/policy/eula/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali Linux Network Service Policy",
+    href: "https://www.kali.org/docs/policy/kali-linux-network-service-policy/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali Linux Open Source Policy",
+    href: "https://www.kali.org/docs/policy/kali-linux-open-source-policy/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali Linux Trademark Policy",
+    href: "https://www.kali.org/docs/policy/trademark/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali Linux Update Policies",
+    href: "https://www.kali.org/docs/policy/kali-linux-security-update-policies/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali Linux User Policy",
+    href: "https://www.kali.org/docs/policy/kali-linux-user-policy/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Kali's Relationship With Debian",
+    href: "https://www.kali.org/docs/policy/kali-linux-relationship-with-debian/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Penetration Testing Tools Policy",
+    href: "https://www.kali.org/docs/policy/penetration-testing-tools-policy/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+  {
+    label: "Privacy Policy",
+    href: "https://www.kali.org/docs/policy/privacy/",
+    author: "Offensive Security",
+    updated: "2024-05-01",
+  },
+];
