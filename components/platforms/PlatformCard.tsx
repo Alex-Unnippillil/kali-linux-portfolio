55fff19c6303fe3972034bd98862d829b71713d5
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-07 19:56:37 -0700

    feat: add platform card component

---
+++ components/platforms/PlatformCard.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+interface PlatformCardProps {
+  title: string;
+  bullets: string[];
+  thumbnail: string;
+}
+
+export default function PlatformCard({ title, bullets, thumbnail }: PlatformCardProps) {
+  return (
+    <div className="flex gap-4 rounded border p-4">
+      <Image src={thumbnail} alt="" width={64} height={64} className="rounded" />
+      <div className="space-y-2">
+        <h3 className="font-bold">{title}</h3>
+        <ul className="list-disc list-inside text-sm space-y-1">
+          {bullets.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
