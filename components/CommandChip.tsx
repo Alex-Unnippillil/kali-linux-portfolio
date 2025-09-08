b00056d9d779e602cff60bc6a8e9a194a5b30f66
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-07 20:56:57 -0700

    feat: enhance command chips

--- components/CommandChip.tsx
+++ components/CommandChip.tsx
@@ -1,25 +1,3 @@
-import React from 'react';
+'use client';
 
-interface CommandChipProps {
-  command: string;
-}
-
-export default function CommandChip({ command }: CommandChipProps) {
-  const copy = async () => {
-    try {
-      await navigator.clipboard.writeText(command);
-    } catch {
-      // ignore clipboard errors
-    }
-  };
-
-  return (
-    <button
-      type="button"
-      onClick={copy}
-      className="px-2 py-0.5 rounded border text-xs font-mono bg-ub-cool-grey text-white hover:bg-ub-orange focus:outline-none focus:ring"
-    >
-      {command}
-    </button>
-  );
-}
+export { default } from './ui/CommandChip';
