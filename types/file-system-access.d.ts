44d88b57c0cea2a639b86a8f712050482ca2e6e2
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-07 20:07:57 -0700

    Auto-merge PR #4888 - chore: add browser api type references
    Force merged PR #4888

---
+++ types/file-system-access.d.ts
@@ -0,0 +1,3 @@
+// MDN: https://developer.mozilla.org/en-US/docs/Web/API/File_System_Access_API
+// GitHub issue: https://github.com/microsoft/TypeScript/issues/29548
+/// <reference types="wicg-file-system-access" />
