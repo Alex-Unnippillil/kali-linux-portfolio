7eadf7a0035eada009e0290ca06e71760937a71c
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-07 19:52:26 -0700

    chore: add browser api type references

---
+++ types/web-bluetooth.d.ts
@@ -0,0 +1,3 @@
+// MDN: https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API
+// GitHub issue: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/49051
+/// <reference types="web-bluetooth" />
