4a2fdca861edd959fc6a087c25ddf38c5f9f37ef
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-09 07:59:46 -0700

    fix: avoid undefined in shuffle

--- apps/games/solitaire/logic.ts
+++ apps/games/solitaire/logic.ts
@@ -31,7 +31,9 @@ export const createDeck = (): Card[] => {
 const shuffle = (deck: Card[]) => {
   for (let i = deck.length - 1; i > 0; i -= 1) {
     const j = Math.floor(random() * (i + 1));
-    [deck[i], deck[j]] = [deck[j], deck[i]];
+    const tmp = deck[i]!;
+    deck[i] = deck[j]!;
+    deck[j] = tmp;
   }
 };
 
