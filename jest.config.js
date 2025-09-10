5910210de86891f7ba408371e58f02e4fa6103e0
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-10 04:04:14 -0400

    Remove duplicate module path and ignore legacy tests

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,11 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^@xterm/xterm/css/xterm.css$': '<rootDir>/__mocks__/styleMock.js',
-    '^@/(.*)$': '<rootDir>/$1',
   },
   testPathIgnorePatterns: [
     '<rootDir>/playwright/',
     '<rootDir>/__tests__/playwright/',
+    '<rootDir>/__tests__/legacy/',
     '<rootDir>/tests/',
   ],
 };
