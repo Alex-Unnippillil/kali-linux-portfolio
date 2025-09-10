46e1844463cd41fe61dfaeca2c9aea68771104ba
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-10 04:03:42 -0400

    refactor: update import path

--- __tests__/desktopDefaultBadge.test.tsx
+++ __tests__/desktopDefaultBadge.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, within } from "@testing-library/react";
-import Home from "../pages/index.jsx";
+import Home from "../pages/index";
 
 describe("Choose your desktop row", () => {
   test("marks Xfce as the default desktop environment", () => {
