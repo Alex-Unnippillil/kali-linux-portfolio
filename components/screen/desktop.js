f59d72ecb4e2243424f2c0b41925ec26777a35bb
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-11 19:37:09 -0400

    feat: dim desktop on maximized window

--- components/screen/desktop.js
+++ components/screen/desktop.js
@@ -839,7 +839,7 @@ export class Desktop extends Component {
             <div className="absolute rounded-md top-1/2 left-1/2 text-center text-white font-light text-sm bg-ub-cool-grey transform -translate-y-1/2 -translate-x-1/2 sm:w-96 w-3/4 z-50">
                 <div className="w-full flex flex-col justify-around items-start pl-6 pb-8 pt-6">
                     <span>New folder name</span>
-                    <input className="outline-none mt-5 px-1 w-10/12  context-menu-bg border-2 border-blue-700 rounded py-0.5" id="folder-name-input" type="text" autoComplete="off" spellCheck="false" autoFocus={true} />
+                    <input aria-label="New folder name" className="outline-none mt-5 px-1 w-10/12  context-menu-bg border-2 border-blue-700 rounded py-0.5" id="folder-name-input" type="text" autoComplete="off" spellCheck="false" autoFocus={true} />
                 </div>
                 <div className="flex">
                     <button
@@ -876,6 +876,10 @@ export class Desktop extends Component {
                 >
                     {this.renderWindows()}
                 </div>
+
+                {this.state.hideSideBar && (
+                    <div className="desktop-overlay" aria-hidden="true" />
+                )}
 
                 {/* Background Image */}
                 <BackgroundImage />
