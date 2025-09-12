ca62fdc26f262417d3a2ac7bbc4a8678e2f865d7
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-12 00:16:48 -0400

    Add safe-area padding for panels and snapped windows

--- components/screen/navbar.js
+++ components/screen/navbar.js
@@ -14,8 +14,15 @@ export default class Navbar extends Component {
 	}
 
 	render() {
-		return (
-                        <div className="main-navbar-vp absolute top-0 right-0 w-screen shadow-md flex flex-nowrap justify-between items-center bg-ub-grey text-ubt-grey text-sm select-none z-50">
+                return (
+                        <div
+                                className="main-navbar-vp absolute top-0 right-0 w-screen shadow-md flex flex-nowrap justify-between items-center bg-ub-grey text-ubt-grey text-sm select-none z-50"
+                                style={{
+                                        paddingTop: 'env(safe-area-inset-top)',
+                                        paddingLeft: 'env(safe-area-inset-left)',
+                                        paddingRight: 'env(safe-area-inset-right)',
+                                }}
+                        >
                                 <div className="pl-3 pr-1">
                                         <Image src="/themes/Yaru/status/network-wireless-signal-good-symbolic.svg" alt="network icon" width={16} height={16} className="w-4 h-4" />
                                 </div>
