3860664a00f2081f274771e9eae2ba5610229caa
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-13 02:04:09 -0400

    [a11y] label icon buttons and hide decorative svgs

--- components/apps/Games/common/VirtualPad.tsx
+++ components/apps/Games/common/VirtualPad.tsx
@@ -16,12 +16,28 @@ export default function VirtualPad({ onDirection, onButton }: PadProps) {
   return (
     <div className="virtual-pad">
       <div className="dpad">
-        <button className="up" onPointerDown={handleDir(0, -1)} />
+        <button
+          className="up"
+          aria-label="Up"
+          onPointerDown={handleDir(0, -1)}
+        />
         <div className="middle">
-          <button className="left" onPointerDown={handleDir(-1, 0)} />
-          <button className="right" onPointerDown={handleDir(1, 0)} />
+          <button
+            className="left"
+            aria-label="Left"
+            onPointerDown={handleDir(-1, 0)}
+          />
+          <button
+            className="right"
+            aria-label="Right"
+            onPointerDown={handleDir(1, 0)}
+          />
         </div>
-        <button className="down" onPointerDown={handleDir(0, 1)} />
+        <button
+          className="down"
+          aria-label="Down"
+          onPointerDown={handleDir(0, 1)}
+        />
       </div>
       <div className="actions">
         <button className="btn-a" onPointerDown={handleBtn('A')}>
