14f53c33ec833e5fd03401854aa67ddf996016d0
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-17 16:40:54 -0400

    feat(desktop): add marquee selection overlay for desktop icons

--- components/base/ubuntu_app.js
+++ components/base/ubuntu_app.js
@@ -31,18 +31,25 @@ export class UbuntuApp extends Component {
     }
 
     render() {
+        const isSelected = Boolean(this.props.isSelected);
+        const baseClasses = "p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-white focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white transition-hover transition-active ";
+        const launchingClass = this.state.launching ? "app-icon-launch " : "";
+        const draggingClass = this.state.dragging ? "opacity-70 " : "";
+        const selectedClass = isSelected ? "bg-opacity-20 ring-2 ring-ub-orange ring-offset-2 ring-offset-transparent " : "";
+
         return (
             <div
                 role="button"
                 aria-label={this.props.name}
                 aria-disabled={this.props.disabled}
+                aria-selected={isSelected ? 'true' : undefined}
                 data-context="app"
                 data-app-id={this.props.id}
+                data-selected={isSelected ? 'true' : undefined}
                 draggable
                 onDragStart={this.handleDragStart}
                 onDragEnd={this.handleDragEnd}
-                className={(this.state.launching ? " app-icon-launch " : "") + (this.state.dragging ? " opacity-70 " : "") +
-                    " p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-white focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white transition-hover transition-active "}
+                className={baseClasses + launchingClass + draggingClass + selectedClass}
                 id={"app-" + this.props.id}
                 onDoubleClick={this.openApp}
                 onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') { e.preventDefault(); this.openApp(); } }}
@@ -64,5 +71,9 @@ export class UbuntuApp extends Component {
         )
     }
 }
+
+UbuntuApp.defaultProps = {
+    isSelected: false,
+};
 
 export default UbuntuApp
