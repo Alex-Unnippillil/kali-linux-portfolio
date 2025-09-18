3710f475094884177a59d3117a4ac0a73c7783c1
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-17 20:07:58 -0400

    Improve drag animations and highlights

--- components/base/ubuntu_app.js
+++ components/base/ubuntu_app.js
@@ -31,6 +31,13 @@ export class UbuntuApp extends Component {
     }
 
     render() {
+        const classes = [
+            this.state.launching ? 'app-icon-launch' : '',
+            'drag-animate',
+            this.state.dragging ? 'drag-animate--active opacity-70' : '',
+            'p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-white focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white transition-hover transition-active',
+        ].filter(Boolean).join(' ');
+
         return (
             <div
                 role="button"
@@ -38,11 +45,11 @@ export class UbuntuApp extends Component {
                 aria-disabled={this.props.disabled}
                 data-context="app"
                 data-app-id={this.props.id}
+                data-desktop-draggable="true"
                 draggable
                 onDragStart={this.handleDragStart}
                 onDragEnd={this.handleDragEnd}
-                className={(this.state.launching ? " app-icon-launch " : "") + (this.state.dragging ? " opacity-70 " : "") +
-                    " p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-white focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white transition-hover transition-active "}
+                className={classes}
                 id={"app-" + this.props.id}
                 onDoubleClick={this.openApp}
                 onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') { e.preventDefault(); this.openApp(); } }}
