456b035c24b143138c204a4954467023923819c4
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-23 23:21:48 -0400

    utils: add browser api feature guards

--- utils/feature.ts
+++ utils/feature.ts
@@ -4,3 +4,9 @@ export const hasOffscreenCanvas = (): boolean =>
   typeof HTMLCanvasElement !== 'undefined' &&
   typeof HTMLCanvasElement.prototype.transferControlToOffscreen === 'function';
 
+export const supportsFSAccess = (): boolean =>
+  typeof window !== 'undefined' && 'showOpenFilePicker' in window;
+
+export const supportsBluetooth = (): boolean =>
+  typeof navigator !== 'undefined' && 'bluetooth' in navigator;
+
