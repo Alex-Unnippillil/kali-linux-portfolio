21ff492056c0c2dc0c4f32547729c9a0e6e6fb91
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-26 05:46:28 -0400

    Update taskbar styling

--- components/screen/taskbar.js
+++ components/screen/taskbar.js
@@ -16,7 +16,7 @@ export default function Taskbar(props) {
     };
 
     return (
-        <div className="absolute bottom-0 left-0 w-full h-10 bg-black bg-opacity-50 flex items-center z-40" role="toolbar">
+        <div className="absolute bottom-0 left-0 w-full h-9 bg-kali-panel/90 border-t border-kali-border flex items-center z-40" role="toolbar">
             {runningApps.map(app => (
                 <button
                     key={app.id}
@@ -25,8 +25,7 @@ export default function Taskbar(props) {
                     data-context="taskbar"
                     data-app-id={app.id}
                     onClick={() => handleClick(app)}
-                    className={(props.focused_windows[app.id] && !props.minimized_windows[app.id] ? ' bg-white bg-opacity-20 ' : ' ') +
-                        'relative flex items-center mx-1 px-2 py-1 rounded hover:bg-white hover:bg-opacity-10'}
+                    className={`relative flex items-center mx-1 py-1 rounded px-2 hover:bg-kali-menu-hover text-kali-text${props.focused_windows[app.id] && !props.minimized_windows[app.id] ? ' bg-white bg-opacity-20' : ''}`}
                 >
                     <Image
                         width={24}
@@ -36,7 +35,7 @@ export default function Taskbar(props) {
                         alt=""
                         sizes="24px"
                     />
-                    <span className="ml-1 text-sm text-white whitespace-nowrap">{app.title}</span>
+                    <span className="ml-1 text-sm whitespace-nowrap">{app.title}</span>
                     {!props.focused_windows[app.id] && !props.minimized_windows[app.id] && (
                         <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-2 h-0.5 bg-white rounded" />
                     )}
