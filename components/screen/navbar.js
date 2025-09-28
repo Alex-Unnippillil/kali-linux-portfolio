b7ac8a78f3a500a8a85b2dc0f3a4c51c99e6fbd2
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-28 12:10:06 -0400

    Adjust navbar clock positioning

--- components/screen/navbar.js
+++ components/screen/navbar.js
@@ -18,19 +18,21 @@ export default class Navbar extends Component {
         }
 
 		render() {
-			return (
-				<div className="main-navbar-vp absolute top-0 right-0 w-screen shadow-md flex flex-nowrap justify-between items-center bg-ub-grey text-ubt-grey text-sm select-none z-50">
-					<div className="flex items-center">
-						<WhiskerMenu />
-						<PerformanceGraph />
-					</div>
-					<div
-						className={
-							'pl-2 pr-2 text-xs md:text-sm outline-none transition duration-100 ease-in-out border-b-2 border-transparent py-1'
-						}
-					>
-						<Clock />
-					</div>
+                        return (
+                                <div className="main-navbar-vp relative top-0 w-screen shadow-md flex flex-nowrap justify-between items-center bg-ub-grey text-ubt-grey text-sm select-none z-50">
+                                        <div className="flex items-center">
+                                                <WhiskerMenu />
+                                                <PerformanceGraph />
+                                        </div>
+                                        <div className="absolute left-1/2 -translate-x-1/2">
+                                                <div
+                                                        className={
+                                                                'pl-2 pr-2 text-xs md:text-sm outline-none transition duration-100 ease-in-out border-b-2 border-transparent py-1'
+                                                        }
+                                                >
+                                                        <Clock />
+                                                </div>
+                                        </div>
 					<button
 						type="button"
 						id="status-bar"
