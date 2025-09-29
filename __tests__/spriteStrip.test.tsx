032f27fb76933807a3f73fa8f5e63d9b7344db46
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-29 04:28:52 -0400

    feat: add zoom controls to sprite preview

--- __tests__/spriteStrip.test.tsx
+++ __tests__/spriteStrip.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { act } from 'react';
 import SpriteStripPreview from '../components/SpriteStripPreview';
 import { importSpriteStrip, clearSpriteStripCache } from '../utils/spriteStrip';
@@ -24,5 +24,21 @@ describe('sprite strip utilities', () => {
       jest.advanceTimersByTime(100);
     });
     expect(el).toHaveStyle('background-position: -10px 0px');
+  });
+
+  test('zoom slider scales the preview', () => {
+    const { getByTestId, getByLabelText } = render(
+      <SpriteStripPreview src="foo.png" frameWidth={10} frameHeight={10} frames={3} fps={10} />,
+    );
+    const el = getByTestId('sprite-strip-preview');
+    const slider = getByLabelText('Zoom') as HTMLInputElement;
+
+    expect(slider.value).toBe('100');
+    expect(el).toHaveStyle('transform: scale(1)');
+
+    fireEvent.change(slider, { target: { value: '150' } });
+
+    expect(slider.value).toBe('150');
+    expect(el).toHaveStyle('transform: scale(1.5)');
   });
 });
