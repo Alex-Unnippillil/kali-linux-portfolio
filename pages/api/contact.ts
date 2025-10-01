d91cc938eb2246e0d51275fb324c636eab1c6f63
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-10-01 17:06:20 -0400

    chore: remove unused analytics import

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,7 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { randomBytes } from 'crypto';
-import trackServerEvent from '@/lib/analytics-server';
-import { reportValue, beta } from '@/flags';
 import { contactSchema } from '../../utils/contactSchema';
 import { validateServerEnv } from '../../lib/validate';
 import { getServiceSupabase } from '../../lib/supabase';
