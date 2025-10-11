128ffa9f9a9e6ffc7359d089b3c8739d6198ec11
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-10-11 04:18:13 -0400

    feat: enhance weather widget parity

--- components/apps/weather_widget.js
+++ components/apps/weather_widget.js
@@ -1,8 +1,12 @@
 import dynamic from 'next/dynamic';
 
+export { fetchWeather } from './weather';
+
 const WeatherWidget = dynamic(() => import('../../apps/weather_widget'), {
   ssr: false,
   loading: () => <p>Loading...</p>,
 });
 
 export default WeatherWidget;
+
+export const displayWeatherWidget = () => <WeatherWidget />;
