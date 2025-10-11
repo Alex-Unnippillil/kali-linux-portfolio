b330e73c72e56faa3c17c3e9b2161d0154d3c91c
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-10-11 10:22:03 -0400

    Fix JSX type reference on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import type { ReactElement } from 'react';
 import Meta from '../components/SEO/Meta';
 import BetaBadge from '../components/BetaBadge';
 import Ubuntu from '../components/ubuntu';
 
-const App = (): JSX.Element => (
+const App = (): ReactElement => (
   <>
     <a href="#window-area" className="sr-only focus:not-sr-only">
       Skip to content
