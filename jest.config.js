122b723327da0a23dd39fffba4203b74e9e561d5
Alex Unnippillil <alex.unnippillil@hotmail.com>
2026-07-23 09:55:34 -0400

    chore: align config scripts with yarn

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^@xterm/xterm/css/xterm.css$': '<rootDir>/__mocks__/styleMock.js',
-    '^@/(.*)$': '<rootDir>/$1',
     '^react-ga4$': '<rootDir>/__mocks__/react-ga4.js',
   },
   testPathIgnorePatterns: [
--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
   webServer: {
-    command: 'npm run dev',
+    command: 'yarn dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
